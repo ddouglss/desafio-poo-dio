35e188a25dc3f93785f351e061c71cfdff87bb58
ddouglss <ddouglss1999@gmail.com>
2025-08-24 01:57:24 -0300

    feat: classes curso e mentoria criadas

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
+
+        List<Curso> cursos = List.of(
+                new Curso("Curso C#", "Curso de C#", 240),
+                new Curso("Curso js", "Curso de js", 120)
+        );
+        cursos.forEach(System.out::println);
+
+        System.out.println("==========================================");
+
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("Curso de Java");
+        curso.setCargaHoraria(120);
+        System.out.println(curso);
+
+        System.out.println("==========================================");
+
+
+        List<Mentoria> mentorias = List.of(
+                new Mentoria("mentoria java", "mentoria de java", LocalDate.now())
+
+        );
+        mentorias.forEach(System.out::println);
+        System.out.println("==========================================");
+
+        mentoria.setTitulo("mentoria C#");
+        mentoria.setDescricao("mentoria de C#");
+        mentoria.setData(LocalDate.now());
+        System.out.println(mentoria);
+
+
     }
 }
